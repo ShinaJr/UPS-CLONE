7cc4961f0d695afb7c4833eed1a51cec0f66b1e5
Moses Oluwashina  Lasisi <lasisioluwashina@gmail.com>
2023-11-16 13:59:39 +0100

    Added safe area context to the screen

--- screens/CustomersScreen.tsx
+++ screens/CustomersScreen.tsx
@@ -1,6 +1,8 @@
-import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import { useTailwind } from 'tailwind-rn';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
 
 const CustomersScreen = () => {
   const tw = useTailwind();
